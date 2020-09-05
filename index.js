3bb8b1c327ac3b9b0ff69abbf55e1fd2fca99153
xaviguasch <xaviguasch@gmail.com>
2020-09-05 18:42:25 +0200

    grabbed all items and added event listeners

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('testing')
+const slides = document.getElementsByClassName('carousel-item')
+
+let slidePosition = 0
+const totalSlides = slides.length
+
+console.log(totalSlides)
+
+const prevBtn = document.getElementById('carousel-button-prev')
+const nextBtn = document.getElementById('carousel-button-next')
+
+prevBtn.addEventListener('click', moveToPrevSlide)
+nextBtn.addEventListener('click', moveToNextSlide)
+
+function moveToPrevSlide() {
+  console.log('move to prev slide!')
+}
+
+function moveToNextSlide() {
+  console.log('move to NEXT slide!')
+}
