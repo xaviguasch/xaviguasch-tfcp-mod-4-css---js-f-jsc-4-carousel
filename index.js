97ca50989398027a06d7bb4679cd041e8865e4e6
xaviguasch <xaviguasch@gmail.com>
2020-09-05 19:21:03 +0200

    added slide functionality

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const slides = document.getElementsByClassName('carousel-item')
 let slidePosition = 0
 const totalSlides = slides.length
 
-console.log(totalSlides)
-
 const prevBtn = document.getElementById('carousel-button-prev')
 const nextBtn = document.getElementById('carousel-button-next')
 
@@ -12,9 +10,25 @@ prevBtn.addEventListener('click', moveToPrevSlide)
 nextBtn.addEventListener('click', moveToNextSlide)
 
 function moveToPrevSlide() {
-  console.log('move to prev slide!')
+  if (slidePosition === 0) {
+    slidePosition = 2
+    slides[0].classList.remove('carousel-item-visible')
+    slides[slidePosition].classList.add('carousel-item-visible')
+  } else if (slidePosition <= totalSlides - 1) {
+    slidePosition--
+    slides[slidePosition + 1].classList.remove('carousel-item-visible')
+    slides[slidePosition].classList.add('carousel-item-visible')
+  }
 }
 
 function moveToNextSlide() {
-  console.log('move to NEXT slide!')
+  if (slidePosition < totalSlides - 1) {
+    slidePosition++
+    slides[slidePosition - 1].classList.remove('carousel-item-visible')
+    slides[slidePosition].classList.add('carousel-item-visible')
+  } else if (slidePosition === totalSlides - 1) {
+    slidePosition = 0
+    slides[totalSlides - 1].classList.remove('carousel-item-visible')
+    slides[slidePosition].classList.add('carousel-item-visible')
+  }
 }
